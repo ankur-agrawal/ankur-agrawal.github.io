f175245cfde61ced72d393343590e711cfdf105d
Ankur Agrawal <ankur.agrawal@medtronic.com>
2021-01-19 20:38:20 -0500

    trying to fix the contact page formatting

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ function readMore()
     var moreText = document.getElementById("more");
     if (moreText.style.display== "none")
     {
-        moreText.style.display ="inline"
+        moreText.style.display = "inline"
     }
     else
     {

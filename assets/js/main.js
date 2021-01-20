b9c699c59ece8ce72de2d648f817b69cacf2d225
Ankur Agrawal <ankur.agrawal@medtronic.com>
2021-01-19 20:14:49 -0500

    added read more functionality

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+function readMore()
+{
+    var moreText = document.getElementById("more");
+    if (moreText.style.display== "none")
+    {
+        moreText.style.display ="inline"
+    }
+    else
+    {
+        moreText.style.display = "none"
+    }
+}
